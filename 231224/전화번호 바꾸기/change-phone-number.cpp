bf3a0323eca8b2094a8b094df24131195f0c4a3a
MsKing <dasung56@gmail.com>
2023-12-24 17:35:05 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231224/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int z;
+    int first, second;
+    char x;
+    cin >> z >> x >> first >> x >> second;
+    cout << "010" << x << second << x << first;
+    return 0;
+}
