af91ae58c7b30d60fdbdf558934644a4e8f713d8
MsKing <dasung56@gmail.com>
2023-12-24 18:02:02 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231224/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
+++ 231224/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -5,7 +5,8 @@ int main() {
     double a,b;
     cin >> a >> b;
     double ans = double(a+b) / double(a-b);
-    cout.precision(3);
+    cout << fixed;
+    cout.precision(2);
     cout << ans;
     return 0;
 }
