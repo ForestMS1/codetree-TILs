5a11e9499a5fd84a02b68647b427ed2d727c74fe
MsKing <dasung56@gmail.com>
2023-12-24 16:34:29 +0900

    Title: 문자 변경하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 231224/문자 변경하기/change-charater.cpp
+++ 231224/문자 변경하기/change-charater.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     char a = 'C';
-    a = 'T'
-    std::cout << a;
+    a = 'T';
+    cout << a;
     return 0;
 }
