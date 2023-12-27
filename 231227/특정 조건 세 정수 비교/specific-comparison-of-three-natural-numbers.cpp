d54bb8b77a1d4878503ba0bdb0b1aaf002e7eb73
MsKing <dasung56@gmail.com>
2023-12-27 21:27:31 +0900

    Title: 특정 조건 세 정수 비교, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231227/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num[3];
+    int min = 0;
+    for(int i = 0; i < 3; i++) {
+        cin >> num[i];
+        if(num[i] < min) {
+            min = num[i];
+        }
+    }
+    if(num[0] == min) {
+        cout << 1 << " ";
+    }
+    else {
+        cout << 0 << " ";
+    }
+    if(num[0] == num[1] && num[1] == num[2]) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+    
+
+
+    return 0;
+}
