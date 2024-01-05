5f1b8fbeff30ec805fec98f65aba63478f23121f
MsKing <dasung56@gmail.com>
2024-01-06 08:00:51 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    bool satisfied = true;
+    for(int i = 2; i < n; i++) {
+        if(n % i == 0) {
+            satisfied = false;
+        }
+    }
+    if(satisfied) {
+        cout << 'P';
+    }
+    else {
+        cout << 'C';
+    }
+    return 0;
+}
