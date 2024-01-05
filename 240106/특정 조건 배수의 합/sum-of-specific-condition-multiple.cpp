108d73147a5b7208904db2406c9ae0b56923ac33
MsKing <dasung56@gmail.com>
2024-01-06 00:04:41 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240106/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -3,9 +3,18 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
+    int max, min;
     cin >> a >> b;
     int sum = 0;
-    for(int i = a; i <= b; i++) {
+    if(a > b) {
+        max = a;
+        min = b;
+    }
+    else {
+        max = b;
+        min = a;
+    }
+    for(int i = min; i <= max; i++) {
         if(i % 5 == 0) {
             sum += i;
         }
