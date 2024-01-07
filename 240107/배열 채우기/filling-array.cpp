ee2c19f4264ba5317c0f95d7d21180beade788cc
MsKing <dasung56@gmail.com>
2024-01-07 19:35:34 +0900

    Title: 배열 채우기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240107/배열 채우기/filling-array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int cnt;
+    for(int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if(arr[i] == 0){
+            cnt = i-1;
+            break;
+        }
+    }
+    for(int i = 0; i <= cnt; i++) {
+        cout << arr[cnt-i] << " ";
+    }
+    return 0;
+}
