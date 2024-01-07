cc3dd35b3b5a936b637fc573566b63f2d813f7ac
MsKing <dasung56@gmail.com>
2024-01-07 18:35:11 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240107/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 240107/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -4,25 +4,16 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int arr[10];
     int sum = 0;
-    int cnt;
+    int cnt = 0;
     double avg;
-    bool satisfied = false;
     for(int i = 0; i < 10; i++) {
         cin >> arr[i];
         if(arr[i] >= 250) {
-            satisfied = true;
-            cnt = i;
-            for(int j = 0; j < i; j++) {
-                sum += arr[j];
-            }
             break;
         }
+        sum += arr[i];
+        cnt++;
     }
-    if(satisfied == false){
-        for(int i = 0; i < 10; i++) {
-            sum += arr[i];
-        }
-    } 
     avg = double(sum) / cnt;
     cout << sum << ' ';
     cout << fixed;
