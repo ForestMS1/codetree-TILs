b8dde6ee4824dac1eadef0747cd702a91c6b469d
MsKing <dasung56@gmail.com>
2024-01-10 21:43:48 +0900

    Title: n개의 숫자 중 최대 2개, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240110/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
+++ 240110/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
@@ -6,18 +6,23 @@ int main() {
     int n;
     cin >> n;
     int arr[100];
-    int max1 = INT_MIN;
-    int max1_idx;
-    int max2 = INT_MIN;
+    int max1, max2;
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
+    }
+    if(arr[0] > arr[1]) {
+        max1 = arr[0];
+        max2 = arr[1];
+    }
+    else {
+        max1 = arr[1];
+        max2 = arr[0];
+    }
+    for(int i = 2; i < n; i++) {
         if(arr[i] > max1) {
             max1 = arr[i];
-            max1_idx = i;
         }
-    }
-    for(int i = 0; i < n; i++) {
-        if(arr[i] > max2 && i != max1_idx) {
+        else if(arr[i] > max2) {
             max2 = arr[i];
         }
     }
