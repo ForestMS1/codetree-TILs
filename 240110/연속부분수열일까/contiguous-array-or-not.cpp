67d69e451affd12689767f15f78b2a50ee719fa7
MsKing <dasung56@gmail.com>
2024-01-10 21:06:42 +0900

    Title: 연속부분수열일까, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240110/연속부분수열일까/contiguous-array-or-not.cpp
+++ 240110/연속부분수열일까/contiguous-array-or-not.cpp
@@ -4,23 +4,24 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n1, n2;
     cin >> n1 >> n2;
-    int A[101];
-    int B[101];
-    for(int i = 1; i <= n1; i++) {
+    int A[100];
+    int B[100];
+    for(int i = 0; i < n1; i++) {
         cin >> A[i];
     }
-    for(int i = 1; i <= n2; i++) {
+    for(int i = 0; i < n2; i++) {
         cin >> B[i];
     }
     bool x = true;
-    for(int i = 1; i <= n1; i++) {
-        for(int j = 1; j <= n2; j++) {
-            if(i+j-1 > n1) {
+    for(int i = 0; i < n1; i++) {
+        for(int j = 0; j < n2; j++) {
+            if(i+j >= n1) {
                 x = false;
                 break;
             }
-            if(A[i+j-1] != B[j]) {
+            if(A[i+j] != B[j]) {
                 x = false;
+                break;
             }
         }
     }
