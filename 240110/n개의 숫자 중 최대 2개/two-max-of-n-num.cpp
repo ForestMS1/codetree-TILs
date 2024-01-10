f35a8b0e0de7835f97cfa0f7a5990648c0d4464f
MsKing <dasung56@gmail.com>
2024-01-10 21:45:12 +0900

    Title: n개의 숫자 중 최대 2개, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
+++ 240110/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
@@ -19,7 +19,8 @@ int main() {
         max2 = arr[0];
     }
     for(int i = 2; i < n; i++) {
-        if(arr[i] > max1) {
+        if(arr[i] >= max1) {
+            max2 = max1;
             max1 = arr[i];
         }
         else if(arr[i] > max2) {
