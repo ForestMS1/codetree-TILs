1549cab183a7b6009d65c1664e0d9e727de072c9
MsKing <dasung56@gmail.com>
2024-01-11 13:54:11 +0900

    Title: 500 근처의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/500 근처의 수/near-500.cpp
+++ 240111/500 근처의 수/near-500.cpp
@@ -4,16 +4,16 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int arr[10];
     int max1 = 0;
-    int max2 = 0;
+    int min2 = 1000;
     for(int i = 0; i < 10; i++) {
         cin >> arr[i];
         if(arr[i] < 500 && arr[i] > max1) {
             max1 = arr[i];
         }
-        if(arr[i] > 500 && arr[i] > max2) {
-            max2 = arr[i];
+        if(arr[i] > 500 && arr[i] < min2) {
+            min2 = arr[i];
         }
     }
-    cout << max1 << ' ' << max2;
+    cout << max1 << ' ' << min2;
     return 0;
 }
