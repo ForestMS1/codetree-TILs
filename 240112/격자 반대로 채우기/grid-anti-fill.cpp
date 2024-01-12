cb4dc51c7da06b403e98ce4a1dae906820a960ea
MsKing <dasung56@gmail.com>
2024-01-12 23:14:07 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/격자 반대로 채우기/grid-anti-fill.cpp
+++ 240112/격자 반대로 채우기/grid-anti-fill.cpp
@@ -6,7 +6,8 @@ int main() {
     cin >> n;
     int arr[10][10];
     int num = 1;
-    for(int i = n-1; i >= 0; i--) {
+    if(n % 2 == 0) {
+        for(int i = n-1; i >= 0; i--) {
         if(i % 2 != 0) {
             for(int j = n-1; j >= 0; j--) {
                 arr[j][i] = num;
@@ -19,6 +20,23 @@ int main() {
                 num++;
             }
         }
+    }
+    }
+    else {
+        for(int i = n-1; i >= 0; i--) {
+        if(i % 2 == 0) {
+            for(int j = n-1; j >= 0; j--) {
+                arr[j][i] = num;
+                num++;
+            }
+        }
+        else {
+            for(int j = 0; j < n; j++) {
+                arr[j][i] = num;
+                num++;
+            }
+        }
+    }
     }
 
     for(int i = 0; i < n; i++) {
