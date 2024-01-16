d05458e9dadc702c66dd9c4ac76290be132239c7
MsKing <dasung56@gmail.com>
2024-01-16 23:22:49 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/Run Length 인코딩/run-length-encoding.cpp
+++ 240116/Run Length 인코딩/run-length-encoding.cpp
@@ -6,40 +6,23 @@ int main() {
     string str;
     cin >> str;
     char x = str[0]; //이전 문자
-    bool seq = false; //연속인가?
     int cnt = 1; //연속된문자개수
-    int sec = 1;//연속된구간개수
-
-    for(int i = 1; i < str.length(); i++) {
-        if(x != str[i]) {
-            sec++;
-        }
-        x = str[i];
-    }
-    cout << sec*2 << '\n';
-    x = str[0];
-
-    if(str.length() == 1) {
-        cout << str[0] << 1;
-        return 0;
-    }
+    string encoded = "";
 
     for(int i = 1; i < str.length(); i++) {
         if(x == str[i]) {
             cnt++;
-            seq = true;
         }
         else {
-            seq = false;
-        }
-        if(seq == false) {
-            cout << x << cnt;
+            encoded += x;
+            encoded += to_string(cnt);
             cnt = 1;
+            x = str[i];
         }
-        if(i == str.length()-1) {
-            cout << str[i] <<cnt;
-        }
-        x = str[i];
     }
+    encoded += x;
+    encoded += to_string(cnt);
+
+    cout << (int) encoded.length() << '\n' << encoded;
     return 0;
 }
