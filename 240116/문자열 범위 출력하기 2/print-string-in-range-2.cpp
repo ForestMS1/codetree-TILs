6e70411021691e3db670bbe2bd20055627209d3c
MsKing <dasung56@gmail.com>
2024-01-16 22:55:50 +0900

    Title: 문자열 범위 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240116/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -7,6 +7,12 @@ int main() {
     int n;
     cin >> str >> n;
     int cnt = 0;
+    if(n > str.length()) {
+        for(int i = str.length()-1; i >= 0; i--) {
+            cout << str[i];
+        }
+        return 0;
+    }
     for(int i = str.length()-1; cnt < n; i--) {
         cout << str[i];
         cnt++;
