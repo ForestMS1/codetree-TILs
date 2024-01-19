8eb3b768f0fa808b0d4c079ede281419270e15b2
MsKing <dasung56@gmail.com>
2024-01-19 10:52:05 +0900

    Title: 문자열 계속 지우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240119/문자열 계속 지우기/keep-removing-string.cpp
+++ 240119/문자열 계속 지우기/keep-removing-string.cpp
@@ -10,6 +10,7 @@ int main() {
     bool same = true;
     for(int i = 0; i < len_A; i++) {
         same = true;
+
         if(A[i] == B[0]) {
             for(int j = 1; j < len_B; j++) {
                 if(A[i+j] != B[j]) {
@@ -17,6 +18,7 @@ int main() {
                     break;
                 }
             }
+
             if(same) {
                 A.erase(i,len_B);
                 i = 0;
