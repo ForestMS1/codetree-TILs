541ae4e6f693fa1107b884c619fefbf3cf90454b
MsKing <dasung56@gmail.com>
2024-01-19 11:03:36 +0900

    Title: e 제거하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/e 제거하기/e-to-remove.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] == 'e') {
+            s.erase(i,1);
+            break;
+        }
+    }
+    cout << s;
+    return 0;
+}
