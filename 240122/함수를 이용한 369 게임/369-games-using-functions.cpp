0df22dcf8f3e3da35f7d94e165dbc872bf1aaecb
MsKing <dasung56@gmail.com>
2024-01-22 15:46:06 +0900

    Title: 함수를 이용한 369 게임, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+bool find369(int n) {
+    while(n%10 != 0) {
+        if(n%10 == 3 || n%10 == 6 || n%10 ==9) {
+            return true;
+        }
+        n = n/10;
+    }
+    return false;
+}
+bool find3n(int n) {
+    return n%3 == 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin >> a >> b;
+    int cnt = 0;
+    for(int i = a; i <= b; i++) {
+        if(find369(i) || find3n(i)) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
