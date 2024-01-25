daa9394e633a7ffd54c10893487c1bf40c70e50b
MsKing <dasung56@gmail.com>
2024-01-25 15:32:21 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string input_str, purpose_str;
+int idx = -1;
+
+int exist_substr(string &is, string &ps) {
+    int is_len = is.length();
+    int ps_len = ps.length();
+    bool is_sub;
+
+    for(int i = 0; i < is_len; i++) {
+        is_sub = true;
+        for(int j = 0; j < ps_len; j++) {
+            if(is[i+j] != ps[j]) {
+                is_sub = false;
+            }
+        }
+        if(is_sub) {
+            idx = i;
+        }
+    }
+
+    return idx;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> input_str >> purpose_str;
+
+    cout << exist_substr(input_str,purpose_str);
+
+    return 0;
+}
