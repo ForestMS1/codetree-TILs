f941dc16caa7c363094cd43e67ebe7e1e2794765
MsKing <dasung56@gmail.com>
2024-01-26 14:18:25 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 19ms, Memory: 6MB, Status: Runtime Error - CodeTree

--- 240126/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 240126/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_N = 10;
+int arr[11];
 
-int arr[MAX_N];
-
-// 최대공약수를 구하는 함수
 int gcdTwonum(int a, int b) {
     int r;
     while (b != 0) {
@@ -16,32 +13,33 @@ int gcdTwonum(int a, int b) {
     return a;
 }
 
-// n개의 수의 최대공약수를 재귀적으로 구하는 함수
 int gcd(int n) {
-    if (n == 2) {
-        return gcdTwonum(arr[0], arr[1]);
+    if(n == 2) {
+        gcdTwonum(arr[0],arr[1]);
     }
-    return gcdTwonum(gcd(n - 1), arr[n - 1]);
+
+    return gcdTwonum(gcd(n-1),arr[n-1]);
 }
 
-// 두 수의 최소공배수를 구하는 함수
 int lcmTwonum(int a, int b) {
-    return (a * b) / gcdTwonum(a, b);
+    return (a*b) / gcdTwonum(a,b);
 }
 
-// n개의 수의 최소공배수를 재귀적으로 구하는 함수
 int lcm(int n) {
-    if (n == 2) {
-        return lcmTwonum(arr[0], arr[1]);
+    if(n == 2) {
+        return lcmTwonum(arr[0],arr[1]);
     }
-    return lcmTwonum(lcm(n - 1), arr[n - 1]);
+
+    return lcmTwonum(lcm(n-1), arr[n-1]);
 }
+
 
 int main() {
+    // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
