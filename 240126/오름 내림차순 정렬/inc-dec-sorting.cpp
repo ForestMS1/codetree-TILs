b88ff8d2e469e8d9086bce216277095791f30107
MsKing <dasung56@gmail.com>
2024-01-26 19:47:06 +0900

    Title: 오름 내림차순 정렬, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int arr[100];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr,arr+n);
+    
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+
+    sort(arr,arr+n,greater<int>());
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    return 0;
+}
