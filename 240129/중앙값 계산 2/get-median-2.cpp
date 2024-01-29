fe53b5f58193080f37aa113775637e4743241d43
MsKing <dasung56@gmail.com>
2024-01-29 16:45:22 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/중앙값 계산 2/get-median-2.cpp
+++ 240129/중앙값 계산 2/get-median-2.cpp
@@ -2,21 +2,19 @@
 #include <algorithm>
 using namespace std;
 
-void printmidnum(int *x, int n) {
-    sort(x,x+n+1);
-    cout << x[n/2+1] << ' ';
-}
+
+int arr[101];
 
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    int arr[101];
     for(int i = 1; i <= n; i++) {
         cin >> arr[i];
         if(i % 2 != 0) {
-            printmidnum(arr, i);
+            sort(arr,arr+i+1);
+            cout << arr[i/2+1] << ' ';
         }
     }
     return 0;
