1768b026ca19f2f8c86bcee3c0fe054d0019eded
daesung <dasung56@gmail.com>
2024-02-10 23:57:17 +0900

    Title: Time to Time, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/Time to Time/time-to-time.cpp
+++ 240210/Time to Time/time-to-time.cpp
@@ -6,37 +6,7 @@ int main() {
     int a,b,c,d;
     cin >> a >> b >> c >> d;
 
-    int hour, mins;
-    hour = 0;
-    mins = 0;
-    int elapsed_time = 0;
-
-    int first_time, last_time;
-
-
-    while(true)
-    {
-        if(hour == a && mins == b)
-        {
-            first_time = elapsed_time;
-        }
-        if(hour == c && mins == d)
-        {
-            last_time = elapsed_time;
-            break;
-        }
-
-        elapsed_time++;
-        mins++;
-
-        if(mins  == 60)
-        {
-            hour++;
-            mins = 0;
-        }
-    }
-
-    cout << last_time - first_time;
+    cout << (c*60+d) - (a*60+b);
 
     return 0;
 }
