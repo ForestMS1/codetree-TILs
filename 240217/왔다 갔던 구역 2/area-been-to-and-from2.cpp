b0476f66ab5863fab0d5fae597c480a1518be193
daesung <dasung56@gmail.com>
2024-02-17 23:35:51 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240217/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int line[2000];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int pos = 1000;
+    int x;
+    char dir;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> x >> dir;
+        if(dir == 'R')
+        {
+            for(int j = 0; j < x; j++)
+            {
+                pos++;
+                line[pos]++;
+            }
+        }
+        else {
+            for(int j = 0; j < x; j++)
+            {
+                pos--;
+                line[pos]++;
+            }
+        }
+    }
+    
+    int cnt = 0;
+    for(int i = 0; i < 2001; i++)
+    {
+        if(line[i] > 1)
+        {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
