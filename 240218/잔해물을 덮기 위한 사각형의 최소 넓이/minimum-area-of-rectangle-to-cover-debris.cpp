2de6e5ebec9df2ea22bc1dac601025415bb0707b
daesung <dasung56@gmail.com>
2024-02-18 18:05:47 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 15ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 240218/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int map[2001][2001];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x1,y1,x2,y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += 1000;
+    x2 += 1000;
+    y1 += 1000;
+    y2 += 1000;
+
+    for(int i = x1; i <= x2; i++)
+    {
+        for(int j = y1; j <= y2; j++)
+        {
+            map[i][j] = 1;
+        }
+    }
+
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += 1000;
+    x2 += 1000;
+    y1 += 1000;
+    y2 += 1000;
+
+    for(int i = x1; i < x2; i++)
+    {
+        for(int j = y1; j < y2; j++)
+        {
+            map[i][j] = 0;
+        }
+    }
+
+    int max_x = 0;
+    int min_x = 3000;
+    int max_y = 0;
+    int min_y = 3000;
+    for(int i = 0; i < 2001; i++)
+    {
+        for(int j = 0; j < 2001; j++)
+        {
+            if(map[i][j] == 1)
+            {
+                if(max_x < i)
+                {
+                    max_x = i;
+                }
+                if(max_y < j)
+                {
+                    max_y = j;
+                }
+                if(min_x > i)
+                {
+                    min_x = i;
+                }
+                if(min_y > j)
+                {
+                    min_y = j;
+                }
+            }
+        }
+    }
+
+    int area = (max_x - min_x) * (max_y - min_y);
+    cout << area;
+    return 0;
+}
