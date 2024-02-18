ad918091f660d32227aa584c9d8d62067724605e
daesung <dasung56@gmail.com>
2024-02-18 17:44:41 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 23ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240218/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+
+int coor[2001][2001];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x1,y1,x2,y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += 1000;
+    x2 += 1000;
+    y1 += 1000;
+    y2 += 1000;
+    for(int i = x1; i < x2; i++)
+    {
+        for(int j = y1; j < y2; j++)
+        {
+            coor[i][j]++;
+        }
+    }
+
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += 1000;
+    x2 += 1000;
+    y1 += 1000;
+    y2 += 1000;
+    for(int i = x1; i < x2; i++)
+    {
+        for(int j = y1; j < y2; j++)
+        {
+            coor[i][j]++;
+        }
+    }
+
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1 += 1000;
+    x2 += 1000;
+    y1 += 1000;
+    y2 += 1000;
+    for(int i = x1; i < x2; i++)
+    {
+        for(int j = y1; j < y2; j++)
+        {
+            coor[i][j] += 2;
+        }
+    }
+
+    int area = 0;
+    for(int i = 0; i <= 2000; i++)
+    {
+        for(int j = 0; j <= 2000; j++)
+        {
+            if(coor[i][j] == 1)
+            {
+                area++;
+            }
+        }
+    }
+    cout << area;
+
+    return 0;
+}
