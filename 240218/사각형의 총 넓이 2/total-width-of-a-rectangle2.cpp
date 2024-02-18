ff56d156ee3cc4b730ab812bab0fb3a02d33610f
daesung <dasung56@gmail.com>
2024-02-18 17:17:52 +0900

    Title: 사각형의 총 넓이 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240218/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int coor[201][201];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N;
+    int x1, y1, x2, y2;
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++)
+    {
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        for(int j = x1; j < x2; j++)
+        {
+            for(int k = y1; k < y2; k++)
+            {
+                coor[j][k]++;
+            }
+        }
+    }
+
+    int area = 0;
+
+    for(int i = 0; i <= 200; i++)
+    {
+        for(int j = 0; j <= 200; j++)
+        {
+            if(coor[i][j] >= 1)
+            {
+                area++;
+            }
+        }
+    }
+    cout << area;
+    return 0;
+}
